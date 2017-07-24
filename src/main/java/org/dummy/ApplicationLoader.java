a93e3eb7ca7c3369aeeb41926ed15a009f135529
Kumar <msundararajan@paypalcorp.com>
2017-07-24 18:07:58 +0530

    initial commit- server Up

---
+++ src/main/java/org/dummy/ApplicationLoader.java
@@ -0,0 +1,13 @@
+package org.dummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLoader {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationLoader.class, args);
+	}
+
+}
